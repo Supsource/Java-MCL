57a54fb6e57cc589a451400400081d65f9455109
Swda <supriyo.mal.org@gmail.com>
2020-10-17 21:07:08 +0530

    if else statement

---
+++ calculator/src/ifelseif.java
@@ -0,0 +1,34 @@
+
+class test1 {
+    public static void main(String[] args) {
+
+  /*    int number = 0;
+      if(number>0){
+          System.out.println("Postive");
+      }else if(number<0){
+          System.out.println("Negative");
+      }else {
+          System.out.println("Zero");
+      } */
+
+        
+
+  /*      int age = 25;
+        int weight = 49;
+        //Applying condition on age and weight
+        if (age > 18) {
+            if (weight > 50) {
+                System.out.println("You are eligible to donate blood");
+            } else if (age < 18) {
+              }  else if (weight < 50) {
+                    System.out.println("You are not eligible to donate blood");
+                } else {
+                    System.out.println("Age must be greater than 18");
+                }
+            }           */
+
+
+
+        }
+    }
+
