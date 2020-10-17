3f8c193f17829735b281291cda5b0a90dd22128b
Swda <supriyo.mal.org@gmail.com>
2020-10-17 18:06:01 +0530

    arrrays

---
+++ calculator/arrays.java
@@ -0,0 +1,11 @@
+public class arrays {
+      public static void main(String[] args) {
+        int [] marks = new int [5];
+        marks[0]= 100;
+        marks[1]= 60;
+        marks[2]= 80;
+        marks[3]= 88;
+        System.out.println(marks[3]);
+        
+    }
+}
