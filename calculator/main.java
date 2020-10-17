472923e5a1ea21748eba4edebd42634ccf2277a3
Swda <supriyo.mal.org@gmail.com>
2020-10-17 17:31:44 +0530

    user input add

---
+++ calculator/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class main{
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the first number");
+        sc.nextInt();
+
+        System.out.println("Enter the second numeber");
+        sc.nextInt();
+
+        System.out.println("Enter the operator");
+        sc.nextLine();
+
+        
+    }
+}
