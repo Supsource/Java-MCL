69f151a9f5f6f4dadf891c3c022b47fa5fd81e83
Swda <supriyo.mal.org@gmail.com>
2020-10-18 14:26:18 +0530

    Update arrays.java

--- calculator/arrays.java
+++ calculator/arrays.java
@@ -1,11 +1,34 @@
 public class arrays {
       public static void main(String[] args) {
-        int [] marks = new int [5];
+
+    /*
+       int [] marks = new int [5];
         marks[0]= 100;
         marks[1]= 60;
         marks[2]= 80;
         marks[3]= 88;
         System.out.println(marks[3]);
-        
+
+          */
+
+
+
+          //Java Nested Switch Statement
+
+
+          char branch = 'c';
+          int collageYear = 4;
+          switch (collageYear)
+          {
+              case 1:
+                  System.out.println("English, Maths, Science");
+              case 2 :
+                  switch (branch){
+                      case 'c':
+                          System.out.println("Operating system");
+                      case 'e':
+
+                  }
+          }
     }
 }
